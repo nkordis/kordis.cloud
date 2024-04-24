76da8d2ffb563eb6ff0ff84aec483d8d2a5285fd
nkordis <nikoskordis@gmail.com>
2024-04-25 00:02:18 +0200

    Set the executable_path explicity

--- tests/e2e/test_frontend.py
+++ tests/e2e/test_frontend.py
@@ -18,7 +18,7 @@ def setup_driver():
 
     try:
         # Create a new instance of the Chrome driver
-        driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
+        driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
         return driver
     except WebDriverException as e:
         print(f"Error initializing the Chrome driver: {str(e)}")
